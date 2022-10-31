8878ce1953140c884d6a7b11e972f504b4601dd3
Heloisatorres <89106934+Heloisatorres@users.noreply.github.com>
2022-10-31 11:24:48 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, VERSION } from '@angular/core';
+import {HttpErrorResponse,HttpResponse} from '@angular/common/http':
+import {GithubService} from './github.service';
+import {Repositorio} from './repositorio';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
+public repositorios: Repositorio[]=[];
+public nomeDeUsuario= '';
+constructor(private github Service: GithubService) {}
+public getRepositorios(): void {
+  this.githubService.getRepositorio(this.nomeDeUsuario).subscribe{
+    next:response: Repositorios=response;
+  },
+  error: (erro:HttpErrorResponse) => {
+    console.error(error.message);
+  },
+}
---
+++ src/app/repositorio.ts
@@ -0,0 +1,6 @@
+export interface Repositorio {
+  id: number;
+  full_name: string;
+  html_url: string;
+  description: string;
+}
